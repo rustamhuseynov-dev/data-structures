9e5877bc9f0d252142b94fefcaf2d50fa90151eb
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-18 10:51:20 +0400

    IdentityHashMap nedir? HashMap ile ne ferqi var?

---
+++ src/main/java/collections/IdentityHashMapTest.java
@@ -0,0 +1,21 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+
+public class IdentityHashMapTest {
+    public static void main(String[] args) {
+        IdentityHashMap<String, String> map = new IdentityHashMap<>();
+        String i = new String("key");
+        String b = new String("key");
+        map.put(i,"Mehemmed");
+        map.put(b,"Mehemmed");
+
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put(i,"Rustam");
+        hashMap.put(b,"Rustam");
+
+        System.out.println(map);
+        System.out.println(hashMap);
+    }
+}
